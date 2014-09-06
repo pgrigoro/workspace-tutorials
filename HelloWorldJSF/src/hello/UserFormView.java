53b6a118e0a5fc1286feabe67cd4980f6253a015
pgrigoro <pgrigoro@yahoo.gr>
2014-09-06 17:11:03 +0300

    initial commit

---
+++ HelloWorldJSF/src/hello/UserFormView.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class UserFormView {
+
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
